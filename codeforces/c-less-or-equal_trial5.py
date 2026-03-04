84b756f19f12f342c75f3bcf12d6263cfe4bef9a
Yabets kifle <111049968+jabez-abija2399@users.noreply.github.com>
2026-03-04 22:13:22 +0300

    Add solution for C - Less or Equal (trial 5)

---
+++ codeforces/c-less-or-equal_trial5.py
@@ -0,0 +1,19 @@
+n, k = map(int, input().split())
+arr = list(map(int, input().split()))
+arr.sort()
+ 
+if k == 0:
+    if arr[0] > 1:
+        print(arr[0] - 1)
+    else:
+        print(-1)
+else:
+    numX = arr[k-1]
+    count = 0
+    for num in arr:
+        if num <= numX:
+            count += 1
+    if count == k:
+        print(numX)
+    else:
+        print(-1)
